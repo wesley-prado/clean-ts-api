6fb6b824c904f599a2ac668c1b65a599a295b248
Wesley Prado <wprado1424@gmail.com>
2020-06-19 00:57:28 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDIr>/src/main/**',
+    '!<rootDIr>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
