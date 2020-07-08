c2eff0d30fc5fd20f0205b54943ba37281de21ff
Wesley Prado <wprado1424@gmail.com>
2020-07-08 00:53:23 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,6 +2,7 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
@@ -15,5 +16,11 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   }
+
 }
