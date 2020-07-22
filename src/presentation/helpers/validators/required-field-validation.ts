7d99538a1c23e103e847f9bd07aabffccf600801
Wesley Prado <wprado1424@gmail.com>
2020-07-22 00:40:32 -0300

    refactor: move password comparation to ValidationComposite

--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -3,6 +3,7 @@ import { MissingParamError } from '../../errors'
 
 export class RequiredFieldValidation implements Validation {
   private readonly fieldName: string
+
   constructor (fieldName: string) {
     this.fieldName = fieldName
   }
